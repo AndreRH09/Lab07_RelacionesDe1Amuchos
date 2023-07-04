83aa74b1f94b59cc80515705b50651758b2ea83f
AndreRH09 <aanazcoh@unsa.edu.pe>
2023-07-04 16:50:39 -0500

    cargando antiguo proyecto

---
+++ proyectoTutorial/calc/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html',{'name': 'Andre'})
+
+
+def add(request):
+
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    resul = val1 + val2
+
+    return render(request, 'result.html', {'res':resul})
