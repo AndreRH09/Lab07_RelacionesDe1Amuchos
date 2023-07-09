d4e402a4eeef8f770c7b623bc205e489d70488fe
AndreRH09 <aanazcoh@unsa.edu.pe>
2023-07-09 11:01:15 -0500

    render2PDF

--- cinemaLog/cinemaLog/views.py
+++ cinemaLog/cinemaLog/views.py
@@ -1,3 +1,17 @@
 from django.http import HttpResponse
 from django.views.generic import View
+from django.template.loader import get_template
 
+from .utils import render_to_pdf
+
+class GeneratePDF(View):
+    def get(self, request,*args, **kawrgs):
+        template=get_template('invoice.html')
+        context= {
+            "invoice_id": 123,
+            "customer_name": "JhonCooper",
+            "amount": 1399.99,
+            "today": "Today",
+        }
+        html=  template.render(context)
+        return HttpResponse(html)
